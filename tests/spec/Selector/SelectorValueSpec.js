cba6351392f0bf84d9eb06e6bb78a5371efb4396
Jens Willmer <mail@jwillmer.de>
2018-02-19 14:45:56 +0100

    Added test to value input element
    #16

---
+++ tests/spec/Selector/SelectorValueSpec.js
@@ -0,0 +1,31 @@
+describe("Value Selector", function () {
+
+    var $el;
+
+    beforeEach(function () {
+    });
+
+    it("should place value in input element", function () {
+
+        var selector = new Selector({
+            id: 'a',
+            type: 'SelectorValue',
+            multiple: false,
+            selector: "#selector-value-input",
+            insertValue: "test"
+        });
+
+        var dataDeferred = selector.getData($("#selector-value"));
+
+        waitsFor(function () {
+            return dataDeferred.state() === 'resolved';
+        }, "wait for data input", 5000);
+
+        runs(function () {
+            dataDeferred.done(function (data) {
+                var input = $("#selector-value-input").val();
+                expect(data[0].a).toEqual(input);
+            });
+        });
+    });
+});
