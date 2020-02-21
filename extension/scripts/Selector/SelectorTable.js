3af4b3b894cd2734fc54eff747848fadc00d9ffd
Alexander <9622929+Yatskov@users.noreply.github.com>
2020-02-21 12:50:31 +0300

    Added more support for vertical tables. (#7)
    * Added feature verticalTable, test tables and correct autofill for Data rows selector
    
    * Added getting header and data raw css for simple tables.
    
    * Fixed header and data row css selector.
    
    * Fixed header and data row css selector.
    
    * Fixed header and data row css selector.
    
    * Some changes of getting data in vertical tables dataCells
    
    * Added DataPreview for vertical Tables
    
    * Added column selection for vertical tables
    
    * Added Data preview and scrape for vertical and horizontal tables without changing Result key
    
    * Added Data preview and scrape for vertical and horizontal tables without changing Result key
    
    * Refactored verticalTable. Now checking doing only in Controller.
    
    * Fixed verticalTable checkbox in SelectorEdit.html
    
    * Fixed a lot of bugs with verticalTable.
    
    * Fixed a lot of bugs of selectorTable, but still have problems with complicated case(table in table)
    
    * Code style changes and moved extraction of selector verticalTable value to getCurrentlyEditedSelector method.

--- extension/scripts/Selector/SelectorTable.js
+++ extension/scripts/Selector/SelectorTable.js
@@ -1,28 +1,29 @@
 var SelectorTable = {
 
-    canReturnMultipleRecords: function () {
+    canReturnMultipleRecords: function() {
         return true;
     },
 
-    canHaveChildSelectors: function () {
+    canHaveChildSelectors: function() {
         return false;
     },
 
-    canHaveLocalChildSelectors: function () {
+    canHaveLocalChildSelectors: function() {
         return false;
     },
 
-    canCreateNewJobs: function () {
+    canCreateNewJobs: function() {
         return false;
     },
-    willReturnElements: function () {
+
+    willReturnElements: function() {
         return false;
     },
-    getTableHeaderColumns: function ($table) {
+
+    getTableHeaderColumns: function($table, verticalTable) {
         var columns = {};
         var headerRowSelector = this.getTableHeaderRowSelector();
         var $headerRow = $table.find(headerRowSelector);
-        var isVerticalRow = false;
 
         if ($headerRow.length > 0) {
             if ($headerRow.length > 1) {
@@ -32,19 +33,16 @@ var SelectorTable = {
                         console.log("%c Please specify row header cell selector ", "background: red; color: white;");
                     }
                 }
-                isVerticalRow = true;
-
             } else if ($headerRow.find("th").length) {
                 $headerRow = $headerRow.find("th");
             } else if ($headerRow.find("td").length) {
                 $headerRow = $headerRow.find("td");
             }
 
-            $headerRow.each(function (i, value) {
+            $headerRow.each(function(i, value) {
                 var header = $(value).text().trim();
                 columns[header] = {
-                    index: i + 1,
-                    isVerticalHeader: isVerticalRow
+                    index: i + 1
                 };
             }.bind(this));
 
@@ -54,17 +52,16 @@ var SelectorTable = {
     },
 
     addMissingColumns(headerRow) {
-        headerRow.each(function (i, value) {
+        headerRow.each(function(i, value) {
             if (this.tableAddMissingColumns) {
                 var header = $(value).text().trim();
-                var column = $.grep(this.columns, function (h) {
+                var column = $.grep(this.columns, function(h) {
                     return h.name === header;
                 });
 
                 if (column.length !== 1) {
                     this.columns.push({
                         header: header,
-                        name: header,
                         extract: true
                     });
                 }
@@ -72,7 +69,7 @@ var SelectorTable = {
         }.bind(this));
     },
 
-    getVerticalDataCells: function (table, dataSelector) {
+    getVerticalDataCells: function(table, dataSelector) {
         var selectors = $(table).find(dataSelector),
             isRow = selectors[0].nodeName === "TR",
             result = [];
@@ -80,25 +77,21 @@ var SelectorTable = {
         if (isRow) {
             console.log("%c Please specify row data cell selector ", "background: red; color: white;");
         } else {
-            for (var i = 0; i < (selectors.length / this.columns.length); i++) {
+            for (var i = 0; i < (selectors.length); i++) {
                 result.push({});
-            };
-
-            selectors.each(function (i, dataCell) {
-                if (dataCell.cellIndex == 0) {
+            }
+            selectors.each(function(i, dataCell) {
+                if (dataCell.cellIndex === 0) {
                     console.log("%c Vertical rows can't have first column as data cell ", "background: red; color: white;");
                 } else {
-                    var index = (dataCell.cellIndex - 1 | dataCell.rowIndex);
-                    var headerCellName = $(dataCell).closest('tr').find("th:first-child").text().trim();
-                    var dataCellvalue = $(dataCell).text().trim();
+                    var headerCellName = $(dataCell).closest("tr").find("th, td")[0].innerText;
 
-                    var extractData = $.grep(this.columns, function (h) {
-                        return h.name === headerCellName && h.extract;
-                    }).length == 1;
-
-                    if (extractData) {
-                        result[index][headerCellName] = dataCellvalue;
+                    var listDataColumnName = this.getDataColumnName(headerCellName);
+                    var dataCellvalue = dataCell.innerText;
+                    if (listDataColumnName) {
+                        result[dataCell.cellIndex - 1][listDataColumnName] = dataCellvalue;
                     }
+
                 }
             }.bind(this));
         }
@@ -106,37 +99,33 @@ var SelectorTable = {
         return result;
     },
 
-    _getData: function (parentElement) {
+    _getData: function(parentElement) {
 
         var dfd = $.Deferred();
-
+        var verticalTable = this.verticalTable;
         var tables = this.getDataElements(parentElement);
 
         var result = [];
-        $(tables).each(function (k, table) {
-            var headerCells = this.getTableHeaderColumns($(table)),
-                dataSelector = this.getTableDataRowSelector(),
-                objKeys = Object.keys(headerCells),
-                headerCellCount = objKeys.length,
-                isVerticalHeader = headerCellCount && headerCells[Object.keys(headerCells)[0]].isVerticalHeader;
+        $(tables).each(function(k, table) {
+            var dataSelector = this.getTableDataRowSelector();
+            if (verticalTable) {
+                var columnsList = this.getVerticalDataCells(table, dataSelector);
+                columnsList.forEach(function(column) {
+                    if (!$.isEmptyObject(column)) {
+                        result.push(column);
+                    }
 
-            if (isVerticalHeader) {
-                var results = this.getVerticalDataCells(table, dataSelector);
-                result.push.apply(result, results);
+                });
             } else {
-                $(table).find(dataSelector).each(function (i, dataCell) {
+                var columnIndices = this.getTableHeaderColumns($(table));
+                $(table).find(dataSelector).each(function(i, dataCell) {
                     var data = {};
 
-                    this.columns.forEach(function (headerCell) {
-                        if (headerCell.extract === true) {
-                            if (headerCells[headerCell.header] === undefined) {
-                                data[headerCell.name] = null;
-                            }
-                            else {
-                                var header = headerCells[headerCell.header];
-                                var rowText = $(dataCell).find(">:nth-child(" + header.index + ")").text().trim();
-                                data[headerCell.name] = rowText;
-                            }
+                    this.columns.forEach(function(column) {
+                        var header = columnIndices[column.header.trim()];
+                        var rowText = $(dataCell).find(">:nth-child(" + header.index + ")").text().trim();
+                        if (column.extract) {
+                            data[column.name] = rowText;
                         }
                     });
                     result.push(data);
@@ -148,10 +137,10 @@ var SelectorTable = {
         return dfd.promise();
     },
 
-    getDataColumns: function () {
+    getDataColumns: function() {
 
         var dataColumns = [];
-        this.columns.forEach(function (column) {
+        this.columns.forEach(function(column) {
             if (column.extract === true) {
                 dataColumns.push(column.name);
             }
@@ -159,117 +148,135 @@ var SelectorTable = {
         return dataColumns;
     },
 
-    getFeatures: function () {
-        return ['selector', 'multiple', 'columns', 'delay', 'tableDataRowSelector', 'tableHeaderRowSelector', 'tableAddMissingColumns']
-    },
+    getDataColumnName: function(header) {
 
-    getItemCSSSelector: function () {
-        return "table";
-    },
+        var answer = this.columns.find(function(column) {
+            return (column.extract && header === column.header.trim());
+        });
+        if (answer) {
+            return answer.name;
+        }
 
-    getTableHeaderRowSelectorFromTableHTML: function (html) {
-        var $table = $(html);
-        var firstRow = $table.find("tr:first-child");
-        var rowCount = $table.find("tr").length;
-        if ($table.find("thead tr:has(td:not(:empty)), thead tr:has(th:not(:empty))").length) {
-
-            if ($table.find("thead tr").length === 1) {
-                return "thead tr";
-            }
-            else {
-                var $rows = $table.find("thead tr");
-                // first row with data
-                var rowIndex = $rows.index($rows.filter(":has(td:not(:empty)),:has(th:not(:empty))")[0]);
-                return "thead tr:nth-of-type(" + (rowIndex + 1) + ")";
-            }
-        }
-        else if (firstRow.find("th:not(:empty)").length > 1) {
-            return "tr:nth-of-type(1)";
-        }
-        else if (firstRow.find("th:first-child:not(:empty)").length == 1 && firstRow.children().length > 1) {
-            return "tr";
-        }
-        else if ($table.find("tr td:not(:empty), tr th:not(:empty)").length) {
-            var $rows = $table.find("tr");
-            // first row with data
-            var rowIndex = $rows.index($rows.filter(":has(td:not(:empty)),:has(th:not(:empty))")[0]);
-            return "tr:nth-of-type(" + (rowIndex + 1) + ")";
-        }
-        else {
-            return "";
-        }
-    },
-
-    getTableDataRowSelectorFromTableHTML: function (html) {
-
-        var $table = $(html);
-        if ($table.find("thead tr:has(td:not(:empty)), thead tr:has(th:not(:empty))").length) {
-
-            return "tbody tr";
-        }
-        else if ($table.find("tr td:not(:empty), tr th:not(:empty)").length) {
-            var $rows = $table.find("tr");
-            // first row with data
-            var rowIndex = $rows.index($rows.filter(":has(td:not(:empty)),:has(th:not(:empty))")[0]);
-            return "tr:nth-of-type(n+" + (rowIndex + 2) + ")";
-        }
-        else {
-            return "";
-        }
-    },
-
-    getTableHeaderRowSelector: function () {
-
-        // handle legacy selectors
-        if (this.tableHeaderRowSelector === undefined) {
-            return "thead tr";
-        }
-        else {
-            return this.tableHeaderRowSelector;
-        }
-    },
-
-    getTableDataRowSelector: function () {
-
-        // handle legacy selectors
-        if (this.tableDataRowSelector === undefined) {
-            return "tbody tr";
-        }
-        else {
-            return this.tableDataRowSelector;
-        }
     },
 
     /**
      * Extract table header column info from html
      * @param html
      */
-    getTableHeaderColumnsFromHTML: function (headerRowSelector, html) {
-
+    getTableHeaderColumnsFromHTML: function(headerRowSelector, html, verticalTable) {
         var $table = $(html);
         var $headerRowColumns = $table.find(headerRowSelector);
 
-        if ($headerRowColumns.length > 1) {
-            $headerRowColumns = $headerRowColumns.find("th:first-child")
-        } else if ($headerRowColumns.find("th").length) {
-            $headerRowColumns = $headerRowColumns.find("th");
-        } else if ($headerRowColumns.find("td").length) {
-            $headerRowColumns = $headerRowColumns.find("td");
-        }
-
         var columns = [];
-
-        $headerRowColumns.each(function (i, columnEl) {
-            var header = $(columnEl).text().trim();
-            var name = header;
-            if (header.length !== 0) {
+        if (!verticalTable) {
+            if ($headerRowColumns.length > 1) {
+                $headerRowColumns = $headerRowColumns.find("th:first-child");
+            } else if ($headerRowColumns.find("th").length) {
+                $headerRowColumns = $headerRowColumns.find("th");
+            } else if ($headerRowColumns.find("td").length) {
+                $headerRowColumns = $headerRowColumns.find("td");
+            }
+        }
+        $headerRowColumns.each(function(i, columnEl) {
+            var header = columnEl.innerText;
+            if (header) {
                 columns.push({
                     header: header,
-                    name: name,
+                    name: header,
                     extract: true
                 });
             }
         });
         return columns;
+    },
+
+    getTableHeaderRowSelectorFromTableHTML: function(html, verticalTable) {
+        var $table = $(html);
+        var firstRow = $table.find("tr:first-child");
+        var rowCount = $table.find("tr").length;
+
+        if ($table.find("thead tr:has(td:not(:empty)), thead tr:has(th:not(:empty))").length) {
+
+            if ($table.find("thead tr").length === 1) {
+                return "thead tr";
+            } else {
+                var $rows = $table.find("thead tr");
+                // first row with data
+                var rowIndex = $rows.index($rows.filter(":has(td:not(:empty)),:has(th:not(:empty))")[0]);
+                return "thead tr:nth-of-type(" + (rowIndex + 1) + ")";
+            }
+        } else {
+            if (!verticalTable) {
+                if (firstRow.find("th:not(:empty)").length > 1) {
+                    return "tr:nth-of-type(1)";
+                } else if (firstRow.find("th:first-child:not(:empty)").length === 1 && firstRow.children().length > 1) {
+                    return "tr";
+                } else if ($table.find("tr td:not(:empty), tr th:not(:empty)").length) {
+                    var $rows = $table.find("tr");
+                    // first row with data
+                    var rowIndex = $rows.index($rows.filter(":has(td:not(:empty)),:has(th:not(:empty))")[0]);
+                    return "tr:nth-of-type(" + (rowIndex + 1) + ")";
+                } else {
+                    return "";
+                }
+            } else {
+                if (firstRow.find("th").length) {
+                    return "tr>th";
+                } else {
+                    return "tr>td:nth-of-type(1)";
+                }
+            }
+
+
+        }
+    },
+
+    getTableDataRowSelectorFromTableHTML: function(html, verticalTable) {
+
+        var $table = $(html);
+        if ($table.find("thead tr:has(td:not(:empty)), thead tr:has(th:not(:empty))").length) {
+
+            return "tbody tr";
+        } else {
+            if (!verticalTable) {
+                if ($table.find("tr td:not(:empty), tr th:not(:empty)").length) {
+                    var $rows = $table.find("tr");
+                    // first row with data
+                    var rowIndex = $rows.index($rows.filter(":has(td:not(:empty)),:has(th:not(:empty))")[0]);
+                    return "tr:nth-of-type(n+" + (rowIndex + 2) + ")";
+                }
+            } else {
+                if ($table.find("th").length) return "tr>td";
+                else return "tr>td:nth-of-type(n+2)";
+            }
+
+        }
+    },
+
+    getFeatures: function() {
+        return ["selector", "multiple", "columns", "delay", "tableDataRowSelector", "tableHeaderRowSelector", "tableAddMissingColumns", "verticalTable"];
+    },
+
+    getItemCSSSelector: function() {
+        return "table";
+    },
+
+    getTableHeaderRowSelector: function() {
+        // handle legacy selectors
+        if (this.tableHeaderRowSelector === undefined) {
+            return "thead tr";
+        } else {
+            return this.tableHeaderRowSelector;
+        }
+    },
+
+    getTableDataRowSelector: function() {
+        // handle legacy selectors
+        if (this.tableDataRowSelector === undefined) {
+            return "tbody tr";
+        } else {
+            return this.tableDataRowSelector;
+        }
     }
+
 };
