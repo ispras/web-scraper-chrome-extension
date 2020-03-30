1a2ff9a9276bf96ab6ea909586f1e7ec65a5b672
GooDRomka <53299930+GooDRomka@users.noreply.github.com>
2020-03-30 15:56:39 +0500

    Fixed submission import sitemap v2.0

--- src/scripts/Controller.js
+++ src/scripts/Controller.js
@@ -531,14 +531,14 @@ export default class SitemapController {
 		$('#file_content').hide();
 		let blob = file.slice(0, file.size);
 		blob.text().then(function(text) {
-			document.getElementById('file_content').value = text;
 			document.getElementById('sitemapJSON').value = text;
+			document.getElementById('sitemapJSON').innerText = text;
 			document.getElementById('edit_sitemap_id').value = '';
 		});
+
 		let sitemapForm = ich.SitemapImport();
 		$('#viewport').html(sitemapForm);
 		this.initImportSitemapValidation();
-		return true;
 	}
 
 	showImportSitemapPanel() {
@@ -640,7 +640,9 @@ export default class SitemapController {
 
 	importSitemap() {
 		// cancel submit if invalid form
-
+		let validator = this.getFormValidator();
+		validator.revalidateField('_id');
+		validator.revalidateField('sitemapJSON');
 		if (!this.isValidForm()) {
 			return false;
 		}
