12a6368d4e5a5951bd9c460876d05390384e247e
GooDRomka <53299930+GooDRomka@users.noreply.github.com>
2020-07-30 13:24:30 +0300

    Added rejects to promises

--- src/background/background.js
+++ src/background/background.js
@@ -112,13 +112,17 @@ browser.runtime.onMessage.addListener(async request => {
 			return browser.tabs.sendMessage(tab.id, request);
 		}
 	} else if (request.backgroundScriptCall) {
-		return new Promise(resolve => {
+		return new Promise((resolve, reject) => {
 			const backgroundScript = getBackgroundScript('BackgroundScript');
 			// TODO change to promises
 			const deferredResponse = backgroundScript[request.fn](request.request);
-			deferredResponse.done(function (resp) {
-				resolve(resp);
-			});
+			deferredResponse
+				.done(function (resp) {
+					resolve(resp);
+				})
+				.reject(err => {
+					reject(err);
+				});
 		});
 	}
 });
--- src/scripts/BackgroundScript.js
+++ src/scripts/BackgroundScript.js
@@ -4,24 +4,22 @@ import * as browser from 'webextension-polyfill';
  * ContentScript that can be called from anywhere within the extension
  */
 let BackgroundScript = {
-	dummy: function() {
-		return $.Deferred()
-			.resolve('dummy')
-			.promise();
+	dummy: function () {
+		return $.Deferred().resolve('dummy').promise();
 	},
 
 	/**
 	 * Returns the id of the tab that is visible to user
 	 * @returns $.Deferred() integer
 	 */
-	getActiveTabId: function() {
+	getActiveTabId: function () {
 		return new Promise((resolve, reject) => {
 			browser.tabs
 				.query({
 					active: true,
 					currentWindow: true,
 				})
-				.then(function(tabs) {
+				.then(function (tabs) {
 					if (tabs.length < 1) {
 						// @TODO must be running within popup. maybe find another active window?
 						reject("couldn't find the active tab");
@@ -38,18 +36,20 @@ let BackgroundScript = {
 	 * @param request.fn	function to call
 	 * @param request.request	request that will be passed to the function
 	 */
-	executeContentScript: function(request) {
+	executeContentScript: function (request) {
 		var reqToContentScript = {
 			contentScriptCall: true,
 			fn: request.fn,
 			request: request.request,
 		};
 		var deferredResponse = $.Deferred();
-		this.getActiveTabId().then(function(tabId) {
-			browser.tabs.sendMessage(tabId, reqToContentScript).then(function(response) {
-				deferredResponse.resolve(response);
-			});
-		});
+		this.getActiveTabId()
+			.then(function (tabId) {
+				browser.tabs.sendMessage(tabId, reqToContentScript).then(function (response) {
+					deferredResponse.resolve(response);
+				});
+			})
+			.reject(err => reject(err));
 
 		return deferredResponse;
 	},
@@ -67,9 +67,9 @@ export default function getBackgroundScript(location) {
 		// if called within background script proxy calls to content script
 		var backgroundScript = {};
 
-		Object.keys(BackgroundScript).forEach(function(attr) {
+		Object.keys(BackgroundScript).forEach(function (attr) {
 			if (typeof BackgroundScript[attr] === 'function') {
-				backgroundScript[attr] = function(request) {
+				backgroundScript[attr] = function (request) {
 					var reqToBackgroundScript = {
 						backgroundScriptCall: true,
 						fn: attr,
@@ -78,9 +78,12 @@ export default function getBackgroundScript(location) {
 
 					var deferredResponse = $.Deferred();
 
-					browser.runtime.sendMessage(reqToBackgroundScript).then(function(response) {
-						deferredResponse.resolve(response);
-					});
+					browser.runtime
+						.sendMessage(reqToBackgroundScript)
+						.then(function (response) {
+							deferredResponse.resolve(response);
+						})
+						.reject(err => reject(err));
 
 					return deferredResponse;
 				};
