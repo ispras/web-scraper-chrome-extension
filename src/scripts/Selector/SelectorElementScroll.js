4782c2067d3691eed630fde558765fb5347cb373
Maksim Varlamov <varlamov@ispras.ru>
2021-03-22 16:34:52 +0300

    scroll to selector

--- src/scripts/Selector/SelectorElementScroll.js
+++ src/scripts/Selector/SelectorElementScroll.js
@@ -1,4 +1,5 @@
 import Selector from '../Selector';
+import ElementQuery from '../ElementQuery';
 
 export default class SelectorElementScroll extends Selector {
 	constructor(options) {
@@ -26,8 +27,15 @@ export default class SelectorElementScroll extends Selector {
 		return true;
 	}
 
-	scrollToBottom() {
-		window.scrollTo(0, document.body.scrollHeight);
+	scroll(parentElement) {
+		if (this.scrollElementSelector) {
+			const scrollElements = ElementQuery(this.scrollElementSelector, parentElement);
+			if (scrollElements.length) {
+				scrollElements[0].scrollIntoView();
+			}
+		} else {
+			window.scrollTo(0, document.body.scrollHeight);
+		}
 	}
 
 	async _getData(parentElement) {
@@ -37,7 +45,7 @@ export default class SelectorElementScroll extends Selector {
 		let foundElements = [];
 
 		// initially scroll down and wait
-		this.scrollToBottom();
+		this.scroll(parentElement);
 		let nextElementSelection = Date.now() + delay;
 
 		return new Promise(resolve => {
@@ -61,11 +69,20 @@ export default class SelectorElementScroll extends Selector {
 					paginationCount++;
 					// continue scrolling and add delay
 					foundElements = elements;
-					this.scrollToBottom();
+					this.scroll(parentElement);
 					nextElementSelection = now + delay;
 				}
 			}, 50);
 		});
+	}
+
+	async afterSelect(cssSelector, controller, inputId) {
+		if (inputId === 'scrollElementSelector') {
+			this.scrollElementSelector = cssSelector;
+		} else {
+			this.selector = cssSelector;
+		}
+		controller._editSelector(this);
 	}
 
 	getDataColumns() {
@@ -73,6 +90,6 @@ export default class SelectorElementScroll extends Selector {
 	}
 
 	getFeatures() {
-		return ['selector', 'multiple', 'delay', 'paginationLimit'];
+		return ['selector', 'scrollElementSelector', 'multiple', 'delay', 'paginationLimit'];
 	}
 }
