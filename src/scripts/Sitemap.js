af1e664989d09c94ffee4fc9df3122d7b7b552df
yatskov <9622929+Yatskov@users.noreply.github.com>
2023-02-09 16:26:21 +0300

    Fix exportSitemap()

--- src/scripts/Sitemap.js
+++ src/scripts/Sitemap.js
@@ -234,9 +234,6 @@ export default class Sitemap {
 		if (sitemapObj['selectors'] === undefined) {
 			sitemapObj['selectors'] = [];
 		}
-		if (sitemapObj['model'] === undefined) {
-			sitemapObj['model'] = [];
-		}
 		return JSON.stringify(sitemapObj, null, 2);
 	}
 
